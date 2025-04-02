7cbc537a6bc5aed4e90d3783933fbd68e011116b
Sasha <sasazmej509@gmail.com>
2025-04-02 22:27:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -35,30 +35,3 @@ class Carnivore(Animal):
             herbivore.health -= 50
             if herbivore.health <= 0:
                 Animal.remove_dead()
-
-
-def test_animal_constructor() -> None:
-    Animal.reset_alive()
-    lion = Animal("Lion King")
-
-    assert hasattr(lion, "name"), \
-        "Animal instance should have attribute 'name'"
-    assert hasattr(lion, "health"), \
-        "Animal instance should have attribute 'health'"
-    assert hasattr(lion, "hidden"), \
-        "Animal instance should have attribute 'hidden'"
-    assert lion.name == "Lion King", \
-        "'lion.name' should equal to 'Lion King'"
-    assert lion.health == 100, \
-        "'lion.health' should equal to 100'"
-    assert lion.hidden is False, "'lion.hidden' should equal to False'"
-
-    print("Current alive animals:", Animal.alive)
-
-    assert len(Animal.alive) == 1, \
-        "Constructor should add created animal to 'Animal.alive'"
-
-
-if __name__ == "__main__":
-    Animal.reset_alive()
-    lion = Carnivore("Simba")
